aeac4221e73495de4997917abd50c78a708e9777
a-pranathi <22f3001788@ds.study.iitm.ac.in>
2025-10-17 21:08:43 +0530

    Initial commit
    	new file:   Dockerfile
    	new file:   config.py
    	new file:   main.py
    	new file:   models.py
    	new file:   requirements.txt

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    GEMINI_API_KEY: str
+    GITHUB_TOKEN: str
+    STUDENT_SECRET: str
+    GITHUB_USERNAME: str
+    STUDENT_EMAIL: str
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+@lru_cache()
+def get_settings():
+    return Settings()
